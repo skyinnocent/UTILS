941d5ca9bb6b6493fb78c38cea3f87db2ec67e71
Syed Masum Reza (sky) <109679731+skyinnocent@users.noreply.github.com>
2023-06-22 19:45:41 +0530

    Update TotalVideoDuration.js
    This is a simple file that helps to find the total duration of videos in a folder. Ubuntu Jammy doesn't come with gui where you can view the total lengeth of video files in a directory. So we can simply use this code and get the total length.

--- TotalVideoDuration.js
+++ TotalVideoDuration.js
@@ -1,1 +1,65 @@
+//////////////////////////////////////////INSTRUCTIONS////////////////////////////////////////
+//////////////////////////////////////////INSTRUCTIONS////////////////////////////////////////
+//////////////////////////////////////////INSTRUCTIONS////////////////////////////////////////
+//////////////////////////////////////////INSTRUCTIONS////////////////////////////////////////
+/**
+ * You must have node.js installed on your system
+ * STEP 1: install the fluent-ffmpeg & moment packages -> Go to terminal paste the below command
+ * npm i -D fluent-ffmpeg moment
+ * Once installed
+ * STEP 2: put the direcory-path where your videos are stored.
+ * You can fnd the directory path simply by opening the directory in your terminal:
+ * --> Go to the folder using gui
+ * --> right click --> Open in terminal --> copy the path --> that will be your path paste it in the
+ * directory variable on line 25;
+ * STEP 3: Run the file by pasting below command in terminal (make sure you are in the correct directory in your terminal)
+ * node TotalVideoTime.js
+ * **/
 
+const fs = require("fs");
+const path = require("path");
+const ffmpeg = require("fluent-ffmpeg");
+const moment = require("moment");
+let totalDuration = moment.duration();
+
+const directory =
+  "/mnt/761CCCA81CCC6529/2025/Courses/[ 0 ][ 1 ]/[TutsNode.com] - React - The Complete Guide (incl Hooks, React Router, Redux)/18 Diving into Redux (An Alternative To The Context API)";
+fs.readdir(directory, (err, files) => {
+  if (err) {
+    console.error("Error reading directory:", err);
+    return;
+  }
+
+  const videoFiles = files.filter((file) => {
+    const extension = path.extname(file).toLowerCase();
+    return [".mp4", ".mkv", ".avi", ".mov"].includes(extension);
+  });
+
+  const filePromises = videoFiles.map((file) => {
+    const filePath = path.join(directory, file);
+
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(filePath, (err, metadata) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        const durationInSeconds = metadata.format.duration;
+        totalDuration.add(durationInSeconds, "seconds");
+        resolve();
+      });
+    });
+  });
+
+  Promise.all(filePromises)
+    .then(() => {
+      console.log(
+        "Total duration:",
+        moment.utc(totalDuration.asMilliseconds()).format("HH:mm:ss")
+      );
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
